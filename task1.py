717c7f99f4c4c59ee62b1d6e9c9ff60b2973eeb5
1ntourist <niyaz090545@gmail.com>
2019-12-16 15:04:25 +0600

    Makers; part_1

---
+++ task1.py
@@ -0,0 +1,30 @@
+"""
+@Makers
+Write a function is_even that takes
+in a number and returns True if it is even,
+otherwise False.
+BONUS CHALLENGE:
+Write the function solution in 1 line
+of code without using if statements.
+"""
+#Make sure to un-comment the function line below when you are done.
+#Remember to name your function is_even()
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+def test_is_even():
+    assert is_even(2) == True
+    assert is_even(3) == False
+    assert is_even(8) == True
+    assert is_even(100) == True
+    assert is_even(101) == False
+    print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_is_even()
+
+# def is_even(num):
+#     return num % 2 == 0
